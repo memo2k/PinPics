0a6e5e9c0be3bead2d0a564ded76f29e99c3b0e8
mehmed <mehmed2kw@gmail.com>
2023-01-11 19:05:19 +0200

    created login and register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className="section-form__auth">
+        <form action="" className="form-auth">
+            <div className="form__header">
+                <div className="form__logo">
+                    <Link to="/">
+                        <img src={logo} alt="logo" width="300" height="75" />
+                    </Link>
+                </div>
+
+                <div className="form__title">
+                    <h2>Login</h2>
+                </div>
+
+                <p>Welcome back!</p>
+            </div>
+
+            <div className="form__body">
+                <div className="form__row">
+                    <div className="form__field">
+                        <input type="email" id="email" className="field" placeholder="Email" />
+                    </div>
+                </div>
+
+                <div className="form__row">
+                    <div className="form__field">
+                        <input type="password" id="password" className="field" placeholder="Password" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="form__actions">
+                <div className="form__btn">
+                    <button type="submit" className="btn btn--auth">Login</button>
+                </div>
+
+                <p>Don't have an acccount? <Link to="/register">Register</Link></p>
+            </div>
+        </form>
+    </section>
   )
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <section className="section-form__auth">
+        <form action="" className="form-auth">
+            <div className="form__header">
+                <div className="form__logo">
+                    <Link to="/">
+                        <img src={logo} alt="logo" width="300" height="75" />
+                    </Link>
+                </div>
+
+                <div className="form__title">
+                    <h2>Register</h2>
+                </div>
+            </div>
+
+            <div className="form__body">
+                <div className="form__row">
+                    <div className="form__field">
+                        <input type="email" className="field" placeholder="Email" />
+                    </div>
+                </div>
+
+                <div className="form__row">
+                    <div className="form__field">
+                        <input type="password" className="field" placeholder="Password" />
+                    </div>
+                </div>
+
+                <div className="form__row">
+                    <div className="form__field">
+                        <input type="password" className="field" placeholder="Confirm password" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="form__actions">
+                <div className="form__btn">
+                    <button type="submit" className="btn btn--auth">Register</button>
+                </div>
+
+                <p>Already have an acccount? <Link to="/login">Login</Link></p>
+            </div>
+        </form>
+    </section>
   )
 }
 
