b35f1f2ebf2b81a939cfa9264bbf22c9c4a0040e
mehmed <mehmed2kw@gmail.com>
2023-01-11 16:34:04 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element="" />
+        <Route path="/" element="" />
+    </Routes>>
+  )
+}
+
+export default App
