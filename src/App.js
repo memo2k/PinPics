a386b723c1bc793ad0321376f5b859b179c92bde
mehmed <mehmed2kw@gmail.com>
2023-01-12 21:02:25 +0200

    added protected routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './container/Home';
+import Profile from './components/Profile';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
-    <div className="wrapper">
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-        </Routes>
-    </div>
+      <AuthContextProvider>
+        <div className="wrapper">
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          </Routes>
+        </div>
+      </AuthContextProvider>
   )
 }
 
