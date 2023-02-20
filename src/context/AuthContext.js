531be90c846b9914649a30528ecc0b538727239d
mehmed <mehmed2kw@gmail.com>
2023-02-20 14:48:31 +0200

    updated AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,7 +3,7 @@ import {
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword, 
     signOut, 
-    onAuthStateChanged 
+    onAuthStateChanged
 } from 'firebase/auth';
 import { auth } from "../firebase";
 
