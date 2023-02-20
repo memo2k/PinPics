66e3775bf4b87ea6c228787ec3556246dad64e30
mehmed <mehmed2kw@gmail.com>
2023-02-20 14:46:05 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import React, { lazy, Suspense, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './container/Home';
-import Profile from './components/Profile';
-import ProtectedRoute from './components/ProtectedRoute';
+
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Home = lazy(() => import("./pages/Home"));
+const Profile = lazy(() => import("./components/Profile/Profile"));
+const ProtectedRoute = lazy(() => import("./components/ProtectedRoute"));
+const Create = lazy(() => import("./components/Create"));
+const PostDetails = lazy(() => import("./components/PostDetails"));
 
 const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
       <AuthContextProvider>
         <div className="wrapper">
-          <Routes>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          </Routes>
+              <Route path="/create" element={<ProtectedRoute><Create /></ProtectedRoute>} />
+              <Route path="/details/:postId" element={<PostDetails />} />
+            </Routes>
+          </Suspense>
         </div>
       </AuthContextProvider>
   )
