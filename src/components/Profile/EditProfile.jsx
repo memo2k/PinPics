6dbf5548bdc80437e291b06ec6df022e55ca54a8
mehmed <mehmed2kw@gmail.com>
2023-02-25 13:44:13 +0200

    updated EditProfile

--- src/components/Profile/EditProfile.jsx
+++ src/components/Profile/EditProfile.jsx
@@ -3,27 +3,27 @@ import { collection, doc, getDoc, getDocs, setDoc, updateDoc } from 'firebase/fi
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import { UserAuth } from '../../context/AuthContext';
 import { db, storage } from '../../firebase';
 import Header from '../Header'
 
 const EditProfile = () => {
     const { userId } = useParams();
+    const { user } = UserAuth();
 
-    // const [value, setValue] = useState({ file: "" });
-    const [user, setUser] = useState({});
+    const [value, setValue] = useState({ name: "", email: "" });
     const [image, setImage] = useState(null);
     const [photoURL, setPhotoURL] = useState(null);
     const [postList, setPostList] = useState([]);
 
     const navigate = useNavigate();
 
-    // Gets user data
+    // Gets user data and posts
     useEffect(() => {
         const getUser = async () => {
             const docData = doc(db, "users", userId);
-
             const docRef = await getDoc(docData);
-            setUser({ ...docRef.data() });
+            setValue({ ...docRef.data() });
         }
 
         const getPostList = async () => {
@@ -33,19 +33,20 @@ const EditProfile = () => {
 
         getUser();
         getPostList();
-    });
+    }, [userId]);
+    
+    // Sets value from the input
+    const handleChange = (name) => (e) => {
+        e.preventDefault();
+        setValue({ ...value, [name]: e.target.value })
+    }
 
     // Function for the Browse button to call the hidden input
     const browseFilesHandler = (e) => {
         e.preventDefault();
         document.getElementById("file").click();
     }
-
-    // const handleChange = (name) => (e) => {
-    //     e.preventDefault();
-    //     setValue({ ...value, [name] : e.target.value })
-    // }
-
+    
     // Previews the selected image
     const handleChangeImage = (e) => {
         e.preventDefault();
@@ -56,24 +57,37 @@ const EditProfile = () => {
         }
     }
 
-    // const { file } = value;
+    const { name, email } = value;
 
+    // Updates the given credentials
     const handleUpdate = async (e) => {
         e.preventDefault();
 
         const storageRef = ref(storage, `users/${userId}/profilePicture`);
 
-        await uploadBytes(storageRef, image);
+        if (image) {
+            await uploadBytes(storageRef, image);
+        }
         
         const newImage = await getDownloadURL(storageRef);
         const docRef = doc(db, "users", userId);
 
-        await updateDoc(docRef, { profilePicture: newImage })
+        await updateDoc(docRef, { 
+            profilePicture: newImage,
+            name: name,
+            email: email
+        })
+
+        await updateProfile(user, {
+            photoURL: newImage,
+            displayName: name
+        })
 
         postList.map(async (post) => {
             if (post.userId == userId) {
                 await updateDoc(doc(db, "posts", post.id), {
-                    authorPicture: newImage
+                    authorPicture: newImage,
+                    author: name
                 });
             }
         })
@@ -97,26 +111,38 @@ const EditProfile = () => {
                             {image ? (
                                 <img src={photoURL} alt="" />
                             ) : (
-                                <img src={user.profilePicture} alt="" />
+                                <img src={value.profilePicture} alt="" />
                             )}
                         </div>
 
                         <div className="form__row-upload">
                             <div className="form__field-button">
-                                <button className="btn btn--browse" onClick={browseFilesHandler}>Browse</button>
+                                <button className="btn btn--browse" onClick={browseFilesHandler}>Change image</button>
                             </div>
 
                             <input type="file" id="file" name="file" onChange={handleChangeImage} hidden />
                         </div>
                     </div>
 
-                    <div className="form__row"></div>
+                    <div className="form__row">
+                        <label htmlFor="name" className="form__label">Username</label>
+                        
+                        <div className="form__field">
+                            <input type="text" id="name" name="name" className="field" onChange={handleChange("name")} value={value.name} />
+                        </div>
+                    </div>
 
-                    <div className="form__row"></div>
+                    <div className="form__row">
+                        <label htmlFor="email" className="form__label">Email</label>
+
+                        <div className="form__field">
+                            <input type="text" id="email" name="email" className="field" onChange={handleChange("email")} value={value.email} />
+                        </div>
+                    </div>
                 </div>
 
                 <div className="form__actions">
-                    <button onClick={handleUpdate} type='submit'>Save</button>
+                    <button onClick={handleUpdate} type='submit' className="btn btn--save">Save</button>
                 </div>
             </form>
         </section>
