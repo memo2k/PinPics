526c9a55154f5e85bfbcd400c8a28f3375026e6d
mehmed <mehmed2kw@gmail.com>
2023-02-25 13:44:48 +0200

    updated Profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,59 @@
-import { doc, getDoc } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { UserAuth } from '../../context/AuthContext';
-import { db } from '../../firebase';
-import Header from '../Header'
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import Masonry from "react-masonry-css";
+import { Link, useParams } from "react-router-dom";
+import { UserAuth } from "../../context/AuthContext";
+import { db } from "../../firebase";
+import Header from "../Header";
+import LikePost from "../LikePost";
 
 const Profile = () => {
   const { userId } = useParams();
   const [userData, setUserData] = useState({});
+  const [postList, setPostList] = useState([]);
+  const [activeBtn, setActiveBtn] = useState("created");
+  const [text, setText] = useState("created");
   const { user } = UserAuth();
 
   useEffect(() => {
     const getUser = async () => {
       const docData = doc(db, "users", userId);
-
       const docRef = await getDoc(docData);
       setUserData({ ...docRef.data() });
-    }
+    };
 
     getUser();
-  });
+  }, [user, userId]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+
+      if (text === "created") {
+        setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        .filter(post => { return post.userId.includes(userId) }));
+      }
+      else if (text === "liked") {
+        setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        .filter(post => { return post.likes.includes(userId) }));
+      }
+    }
+
+    getPosts();
+  }, [postList, userId]);
+
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    540: 1
+}
 
   return (
     <>
       <Header />
-      
+
       <section className="profile">
         <div className="shell">
           <div className="profile__inner">
@@ -34,28 +63,94 @@ const Profile = () => {
               </div>
 
               <div className="profile__username">
-                <h1>
-                  {userData.name}
-                </h1>
+                <h1>{userData.name}</h1>
               </div>
 
               <div className="profile__edit">
-                {user && (
-                  (user.uid === userId) && (
-                    <Link to={`/edit-profile/${userId}`} className="btn btn--edit">Edit Profile</Link>
-                  )
+                {user && user.uid === userId && (
+                  <Link
+                    to={`/edit-profile/${userId}`}
+                    className="btn btn--edit"
+                  >
+                    Edit Profile
+                  </Link>
                 )}
               </div>
             </div>
 
             <div className="profile__content">
+              <div className="profile__nav">
+                <button type="button" onClick={(e) => {
+                  setText("created")
+                  setActiveBtn("created")
+                  }} className={`${activeBtn === 'created' ? 'btn btn--profile-content btn--active' : 'btn btn--profile-content' }`}>Created</button>
 
+                <button type="button" onClick={(e) => {
+                  setText("liked")
+                  setActiveBtn("liked")
+                  }} className={`${activeBtn === 'liked' ? 'btn btn--profile-content btn--active' : 'btn btn--profile-content' }`}>Liked</button>
+              </div>
+
+              <div className="profile__posts">
+                <Masonry
+                  breakpointCols={breakpoints}
+                  className="my-masonry-grid"
+                  columnClassName="my-masonry-grid_column"
+                >
+                  {postList
+                    .sort((a, b) => a.timeStamp < b.timeStamp ? 1 : -1)
+                    .map((post, id) => (
+                      <div className="post" key={id + 100}>
+                        <Link to={`/details/${post.id}`}>
+                          <div className="post__image">
+                            <LazyLoadImage src={post.imageUrl}
+                            placeholderSrc={post.imageUrl}
+                            wrapperProps={{ style: { display: 'block' } }} 
+                            effect="blur" id="image" alt='image' />
+                          </div>
+                        </Link>
+
+                        <div className="post__details">
+                          <div className="author">
+                            <div className="author__image">
+                              <Link to={`/profile/${post.userId}`}>
+                                <img src={post.authorPicture} alt="pfp" />
+                              </Link>
+                            </div>
+
+                            <div className="author__name-white">
+                              <Link to={`/profile/${post.userId}`}>
+                                {post.author}
+                              </Link>
+                            </div>
+                          </div>
+
+                          <div className="post__actions">
+                            {post.likes.length === 1 ? (
+                              <div className="post__actions-likes">
+                                {post.likes.length} like
+                              </div>
+                            ) : (
+                              <div className="post__actions-likes">
+                                {post.likes.length} likes
+                              </div>
+                            )}
+
+                            {user && (
+                              <LikePost id={post.id} likes={post.likes} />
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                </Masonry>
+              </div>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
