9eea2e09efbd71c1fd54c9d5f5814e64885df5cd
mehmed <mehmed2kw@gmail.com>
2023-03-14 14:51:45 +0200

    added Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <section className="section-loader">
+        <span className="loader loader--medium"></span>
+    </section>
+  )
+}
+
+export default Loading
