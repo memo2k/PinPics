d136fcf7c98108da465f12c9eda444532b558776
mehmed <mehmed2kw@gmail.com>
2023-03-14 14:51:54 +0200

    updated Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -37,56 +37,63 @@ const Post = () => {
   }
 
   return (
-    <Masonry
-        breakpointCols={breakpoints}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-    >
-        {postList
-        .sort((a, b) => a.timeStamp < b.timeStamp ? 1 : -1)
-        .map((post, id) => (
-          <div className="post" key={id + 100}>
-            <Link to={`/details/${post.id}`}>
-              <div className="post__image">
-                <LazyLoadImage src={post.imageUrl}
-                placeholderSrc={post.imageUrl}
-                wrapperProps={{ style: { display: 'block' } }} 
-                effect="blur" id="image" alt='image' />
-              </div>
-            </Link>
-  
-            <div className="post__details">
-              <div className="author">
-                <div className="author__image">
-                  <Link to={`/profile/${post.userId}`}>
-                    <img src={post.authorPicture} alt="pfp" />
-                  </Link>
-                </div>
-  
-                <div className="author__name-white">
-                  <Link to={`/profile/${post.userId}`}>{post.author}</Link>
-                </div>
-              </div>
-  
-              <div className="post__actions">
-                {(post.likes.length === 1) ? (
-                  <div className="post__actions-likes">
-                    {post.likes.length} like
+    <>
+      {postList.length > 0 ? (
+        <Masonry
+            breakpointCols={breakpoints}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+        >
+            {postList
+            .sort((a, b) => a.timeStamp < b.timeStamp ? 1 : -1)
+            .map((post, id) => (
+              <div className="post" key={id + 100}>
+                <Link to={`/details/${post.id}`}>
+                  <div className="post__image">
+                    <LazyLoadImage src={post.imageUrl}
+                    placeholderSrc={post.imageUrl}
+                    wrapperProps={{ style: { display: 'block' } }} 
+                    effect="blur" id="image" alt='image' />
                   </div>
-                ) : (
-                  <div className="post__actions-likes">
-                    {post.likes.length} likes
+                </Link>
+      
+                <div className="post__details">
+                  <div className="author">
+                    <div className="author__image">
+                      <Link to={`/profile/${post.userId}`}>
+                        <img src={post.authorPicture} alt="pfp" />
+                      </Link>
+                    </div>
+      
+                    <div className="author__name-white">
+                      <Link to={`/profile/${post.userId}`}>{post.author}</Link>
+                    </div>
                   </div>
-                )}
-  
-                {user && (
-                  <LikePost id={post.id} likes={post.likes} />
-                )}
+      
+                  <div className="post__actions">
+                    {(post.likes.length === 1) ? (
+                      <div className="post__actions-likes">
+                        {post.likes.length} like
+                      </div>
+                    ) : (
+                      <div className="post__actions-likes">
+                        {post.likes.length} likes
+                      </div>
+                    )}
+      
+                    {user && (
+                      <LikePost id={post.id} likes={post.likes} />
+                    )}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
-    </Masonry>
+            ))}
+        </Masonry>
+
+      ) : (
+        <div className="no-posts">There is nothing here...</div>
+      )}
+    </>
   )
 }
 
