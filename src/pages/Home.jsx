66233097562b7b51cc917a1684db4261da6880d1
mehmed <mehmed2kw@gmail.com>
2023-03-14 14:52:41 +0200

    updated Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Post from '../components/Post';
 import heroImage from '../assets/hero.jpg';
 import { UserAuth } from '../context/AuthContext';
+import Footer from '../components/Footer';
 
 const Home = () => {
     const { user } = UserAuth();
@@ -11,28 +12,27 @@ const Home = () => {
         <>
             <Header />
 
-            <section className="hero" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${heroImage})` }}>
-                <div className="shell">
-                    <div className="hero__inner">
-                        <div className="hero__title">
-                            <h1>PinPics</h1>
-                        </div>
-
-                        <div className="hero__description">
-                            <p>PinPics provides high quality and completely free stock photos.
-                            All photos are nicely tagged, searchable and also easy to discover through our discover pages.</p>
+            <div className="home">
+                <section className="hero" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${heroImage})` }}>
+                    <div className="shell">
+                        <div className="hero__inner">
+                            <div className="hero__title">
+                                <h1>PinPics</h1>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
-            <section className="posts">
-                <div className="shell">
-                    <div className="posts__inner">
-                        <Post />
+                <section className="posts">
+                    <div className="shell">
+                        <div className="posts__inner">
+                            <Post />
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
+
+            <Footer />
         </>
     )
 }
